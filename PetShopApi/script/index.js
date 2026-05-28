4e34ed074a464e3d6096f0bd52ec1266b212ee33
amorandy <antoniomorandy@hotmail.com>
2026-05-28 10:29:54 -0400

    revision iniciarsesion del javascript

--- PetShopApi/script/index.js
+++ PetShopApi/script/index.js
@@ -50,7 +50,13 @@ async function loginUsuario(email, password) {
 async function iniciarSesion() {
     const email = document.getElementById("loginEmail").value;
     const password = document.getElementById("loginPass").value;
-
+    
+    if (!email || !password) {
+        alert("Por favor, completa todos los campos.");
+        return;
+    }
+    console.log("Email capturado:", email);
+    console.log("Password capturado:", password);
     try {
         const response = await fetch(`${CONFIG.API_BASE_URL}/usuarios/login`, {
             method: 'POST',
