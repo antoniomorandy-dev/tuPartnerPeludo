cdec32ea370ee45bad0c870ea3fa1f3b9409e0e7
amorandy <antoniomorandy@hotmail.com>
2026-06-01 10:04:43 -0400

    cambio config.js

--- PetShopApi/script/config.js
+++ PetShopApi/script/config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    API_BASE_URL: "https://calculate-external-transportation-goes.trycloudflare.com" 
+    API_BASE_URL: "https://relax-mississippi-fragrance-cyber.trycloudflare.com" 
 };
